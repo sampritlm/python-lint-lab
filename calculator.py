c0b5f507ceb0c6b0cb0d1f12eb8af60dd4de77ad
Samprit <samprit235@gmail.com>
2026-03-20 22:13:28 +0530

    Add calculator and dev requirements

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""Simple calculator module"""
+
+def add(a, b):
+    """Return sum of two numbers"""
+    return a + b
+
+def subtract(a, b):
+    """Return difference of two numbers"""
+    return a - b
